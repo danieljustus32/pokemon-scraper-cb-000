fe5946198302340d063c4970e8774bd562c3876c
Daniel Justus <daniel.justus32@gmail.com>
2017-12-02 23:25:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
   end
 
   def alter_hp(value, db)
-    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?', value, self.class.find.id)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?';, value, self.class.find.id)
     binding.pry
   end
 end
