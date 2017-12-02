0afbf91429145c325d314ef20188da14aa23b263
Daniel Justus <daniel.justus32@gmail.com>
2017-12-02 23:08:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,8 +5,16 @@ class Pokemon
   def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
-    @type = type 
+    @type = type
     @db = db
-    @hp = hp 
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+
+  end
+
+  def self.find(id, db)
+
   end
 end
