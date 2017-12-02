32be27dbf98b4fffec5815693b983039143fe246
Daniel Justus <daniel.justus32@gmail.com>
2017-12-02 23:07:26 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,17 +5,8 @@ class Pokemon
   def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
-    @type = type
-    @hp = hp
-  end
-
-  def self.save(name, type, db)
-    db.execute('INSERT INTO pokemon(name, type) VALUES (?, ?)', name, type)
-  end
-
-  def self.find(id, db)
-    db_data = db.execute('SELECT * FROM pokemon WHERE id = ?', id)
-    attrs = db_data[0]
-    self.new(id: attrs[0], name: attrs[1], type: attrs[2], db: db)
+    @type = type 
+    @db = db
+    @hp = hp 
   end
 end
